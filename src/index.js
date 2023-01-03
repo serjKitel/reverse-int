6838399e2837ae189dfa4732b742aa3cff144f6b
serjKitel <kitselsiarheivladimirovich@gmail.com>
2023-01-03 18:37:49 +0300

    feat: add script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+    let reversedInt = "";
+    for(let i = String(n).length - 1; i >= 0 ; i--){
+      reversedInt += n.toString()[i];
+    }
+    return parseInt(reversedInt);
+  
   
 }
